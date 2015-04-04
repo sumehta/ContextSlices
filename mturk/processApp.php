da4b627f672adbac1d61fdcd718efdf3bb6adc5b
Kurt Luther <kurt@kurtluther.com>
2015-04-04 13:37:20 -0400

    terminate or redirect to review page based on num entries in DB

--- mturk/processApp.php
+++ mturk/processApp.php
@@ -10,8 +10,6 @@ $workerID = $_POST['workerId'];
 $img_id=$_POST['img'];
 $endpoint = $_POST['endpoint'];
 
-
-
 // connect to database
 require_once './mysql.php';
 
@@ -28,9 +26,24 @@ $q = sprintf(" INSERT INTO app_db (worker_id, assignment_id, description, locati
     );
 mysql_query($q);
 
-// redirect us to second page for additional information
-header('Location: app_db_2.php?img_id='.$img_id);
+// fetch number of entries in database
+// see if we have enough to push worker to review page
+// otherwise they're done
 
-// redirect us to submit to MTurk
-// header('Location: completed.php?assignmentId='.$assignmentID);
+$minNumEntries = 5; // should correspond to num shown on review page
 
+$q = sprintf(" SELECT * FROM app_db
+    WHERE img_id = '%s'
+    AND assignment_id != '%s' ",
+    $img_id,
+    $assignmentID
+    );
+$result = mysql_query($q);
+if(mysql_num_rows($result) >= $minNumEntries) {
+    // we have enough entries
+    // redirect us to review page
+    header('Location: app_db_2.php?img_id='.$img_id.'&assignment_id='.$assignmentID);
+} else {
+    // redirect us to submit to MTurk
+    header('Location: completed.php?assignmentId='.$assignmentID);  
+}
