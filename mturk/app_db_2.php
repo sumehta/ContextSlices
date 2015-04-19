1f5e398590e5af6bb824711f139dc8a2fdc22612
Joe Fletcher <joevt@vt.edu>
2015-04-19 09:31:59 -0400

    fixed merge conflict

--- mturk/app_db_2.php
+++ mturk/app_db_2.php
@@ -17,13 +17,13 @@ $assignment_Id=$_GET['assignment_Id'];
 include 'mysql.php';
 
 //Get an description array. Get the number of rows in the table.
+
 //ORDER by confidence
 $sth1 = mysql_query("SELECT assignment_id, description, location, img_id FROM app_db ORDER BY confidence DESC");
 //ORDER by RAND
 //$sth1 = mysql_query("SELECT assignment_id, description, location, img_id FROM app_db ORDER BY RAND()");
-// $rowCounter = mysql_num_rows();
+
 $i = 0;
-//$describeInfo = array();
 
   $table = '<table border="1" cellpadding="10">';
   $table .= '<tr><td><b>' . "Results for 'Description'" . '</b></td>';
@@ -31,14 +31,14 @@ $i = 0;
   $table .= '<td><b>' . "Review the Location" . '</b></td></tr>';
 
 while($r = mysql_fetch_assoc($sth1)) {
-		
-	$assId = $r['assignment_id'];
-	$describeInfo = $r['description'];
-	$locInfo = $r['location'];
-	$imgInfo = $r['img_id'];
-	
-	if($imgInfo == $img_id && $assId!=$assignment_Id && $i < 5)
-	{       
+    
+  $assId = $r['assignment_id'];
+  $describeInfo = $r['description'];
+  $locInfo = $r['location'];
+  $imgInfo = $r['img_id'];
+  
+  if($imgInfo == $img_id && $assId!=$assignment_Id && $i < 5)
+  {       
       $table .= '<tr><td>' . $describeInfo . '</td>';
       $table .= '<td>' . $locInfo . '</td>';
       $table .= '<td>
@@ -48,7 +48,7 @@ while($r = mysql_fetch_assoc($sth1)) {
             <div><input type="radio" name="radio' . $i . '" value="4 ' . $assId . '">Definitely right</div>
             </td></tr>';
       $i = $i + 1;      
-	}
+  }
 }
   $table .= '</tr></table>';
 
@@ -65,15 +65,15 @@ while($r = mysql_fetch_assoc($sth1)) {
 <BR>
 
 
-	<input type="hidden" name="rowNumber" value="<?= $i ?>" />
-	<!-- 
-	<input type="hidden" name="reviewId1" value="<?= $reviewID[0] ?>" />
-	<input type="hidden" name="reviewId2" value="<?= $reviewID[1] ?>" />
-	<input type="hidden" name="reviewId3" value="<?= $reviewID[2] ?>" />
-	<input type="hidden" name="reviewId4" value="<?= $reviewID[3] ?>" />
-	<input type="hidden" name="reviewId5" value="<?= $reviewID[4] ?>" />
+  <input type="hidden" name="rowNumber" value="<?= $i ?>" />
+  <!-- 
+  <input type="hidden" name="reviewId1" value="<?= $reviewID[0] ?>" />
+  <input type="hidden" name="reviewId2" value="<?= $reviewID[1] ?>" />
+  <input type="hidden" name="reviewId3" value="<?= $reviewID[2] ?>" />
+  <input type="hidden" name="reviewId4" value="<?= $reviewID[3] ?>" />
+  <input type="hidden" name="reviewId5" value="<?= $reviewID[4] ?>" />
  -->
-	
+  
     <input type="hidden" name="assignmentId" value="<?= $_GET['assignment_Id'] ?>" />
     <input type="hidden" name="workerId" value="<?= $_GET['workerId'] ?>" />
     <input type="hidden" name="img" value="<?= $img_id ?>" />
