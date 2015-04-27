dca65806ec47b27c36babbc8b5edac89bb3e687c
andychen23 <andychenet@gmail.com>
2015-04-27 14:38:53 -0400

    4/27. app_db_2.php update
    app_db_2.php In order to make each assignment has equally reviewed times
    as possible. I change the review grading table to show 5 assignments
    that have been least reviewed.

--- mturk/app_db_2.php
+++ mturk/app_db_2.php
@@ -65,45 +65,66 @@ $(document).ready(function(){
     </tr>
 
 <?php
-
-//Get an description array. Get the number of rows in the table.
-
-
-//ORDER by confidence
-$q = sprintf(" SELECT * FROM app_db
+//Select all assigmentIds in the app_db and count how many times they have been reviewed in the appreview_db.
+$q2 = sprintf(" SELECT * FROM app_db
     WHERE img_id = '%s'
-    AND assignment_id != '%s'
-    ORDER BY RAND()
-    LIMIT 5 ",
+    AND assignment_id != '%s' ",
     $img_id,
     $assignment_Id,
     $endpoint
     );
-
-$sth1 = mysql_query($q);
-
-//ORDER by RAND
-//$sth1 = mysql_query("SELECT assignment_id, description, location, img_id FROM app_db ORDER BY RAND()");
-
-$i = 0;
-
-while($r = mysql_fetch_assoc($sth1)) {
+	$sth2 = mysql_query($q2);
+	
+	$count=array();
+	$countAssId = array();
+	$countDescribeInfo = array();
+	$countLocInfo = array();
+	$countConfInfo = array();
+	$countImgInfo = array();
+	//Use array to store all the information
+	while($r2 = mysql_fetch_assoc($sth2)) {
     
-  $assId = $r['assignment_id'];
-  $describeInfo = $r['description'];
-  $locInfo = $r['location'];
-  $confInfo = $r['why'];
-  $imgInfo = $r['img_id'];
+	$countAssId[] = $r2['assignment_id'];
+	$countDescribeInfo[] = $r2['description'];
+	$countLocInfo[] = $r2['location'];
+	$countConfInfo[] = $r2['why'];
+	$countImgInfo[] = $r2['img_id'];
+	
+	$results = mysql_query("SELECT * FROM appreview_db WHERE beReviewer_id='{$r2['assignment_id']}'");
+	//$count is an array store the number of times the 'assignment_id' been reviewed in appreview_db.
+	
+	$count[]= mysql_num_rows($results);
+	
+	}
+	
+	//$list5 is an array to sort the index in $count by the value of $count. 
+	$list5=array();
+	asort($count);
+	foreach ($count as $key => $val) 
+	{
+    $list5[]=$key; 
+	}
+	
+	$i = 0;
+
+	while($i<5) {
+    
+	$assId = $countAssId[$list5[$i]];
+	$describeInfo = $countDescribeInfo[$list5[$i]];
+	$locInfo = $countLocInfo[$list5[$i]];
+	$confInfo = $countConfInfo[$list5[$i]];
+	$imgInfo = $countImgInfo[$list5[$i]];
       
       echo '<tr>';
       echo '<td>' . $describeInfo . '</td>';
       echo '<td>' . $locInfo . '</td>';
       echo '<td>' . $confInfo .'</td>';
       echo '<td>
-            <label><input type="radio" name="radio' . $i . '" value="1 ' . $assId . '"> Definitely wrong</label>
-            <label><input type="radio" name="radio' . $i . '" value="2 ' . $assId . '"> Probably wrong</label>
-            <label><input type="radio" name="radio' . $i . '" value="3 ' . $assId . '"> Probably right</label>
             <label><input type="radio" name="radio' . $i . '" value="4 ' . $assId . '"> Definitely right</label>
+			<label><input type="radio" name="radio' . $i . '" value="3 ' . $assId . '"> Probably right</label>
+			<label><input type="radio" name="radio' . $i . '" value="2 ' . $assId . '"> Probably wrong</label>
+			<label><input type="radio" name="radio' . $i . '" value="1 ' . $assId . '"> Definitely wrong</label>
+             
             </td>';
       echo '</tr>';
       $i++;      
